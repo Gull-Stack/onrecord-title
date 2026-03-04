e833e21dd6bac627346fe459ddb5e2dbe8b8273a
Bryce Morgan <brycedmorgan@gmail.com>
2026-03-04 16:59:40 -0700

    Bot Melvin: v3 Editorial Light rebuild — full site redesign, new CSS, all pages rebuilt

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("currentYear", () => new Date().getFullYear());
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
